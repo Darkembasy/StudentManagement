56e2a045d9c871cfe6af0417d196a4ca1fe94537
Yape,JohnreyJ. <136978348+Darkembasy@users.noreply.github.com>
2025-05-21 15:42:49 +0800

    Rename name to first_name

--- StudentManagementSystem/app/models.py
+++ StudentManagementSystem/app/models.py
@@ -10,7 +10,7 @@ class Student(models.Model):
     email = models.EmailField()
 
     def __str__(self):
-        return self.name
+        return self.first_name
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
