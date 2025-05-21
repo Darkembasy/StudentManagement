bd15c0f0d4028b5a6b256fe9d953ced243ae9929
Yape,JohnreyJ. <136978348+Darkembasy@users.noreply.github.com>
2025-05-21 14:09:38 +0800

    Adding Static Files

--- StudentManagementSystem/app/urls.py
+++ StudentManagementSystem/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexViewPage
+from .views import indexViewPage, studentViewPage
 
 urlpatterns = [
     path('',indexViewPage.as_view(),name='index'),
+    path('student/',studentViewPage.as_view(),name='student'),
 ]
--- StudentManagementSystem/app/views.py
+++ StudentManagementSystem/app/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class indexViewPage(TemplateView):
        template_name = 'app/index.html'
+
+class studentViewPage(TemplateView):
+       template_name = 'app/studentList.html'
