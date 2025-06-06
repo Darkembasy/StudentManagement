0ca503a767dd1cce04c978753666a60124a1bef8
Yape,JohnreyJ. <136978348+Darkembasy@users.noreply.github.com>
2025-06-07 01:46:53 +0800

    Update settings.py

--- StudentManagementSystem/settings.py
+++ StudentManagementSystem/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'app/staticfiles'
+STATIC_ROOT = BASE_DIR / 'app/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
