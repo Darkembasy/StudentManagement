dc4500e8b8f0f4f6e7d2570fbb7b7943e164d24a
Yape,JohnreyJ. <136978348+Darkembasy@users.noreply.github.com>
2025-06-07 02:10:59 +0800

    Fixing Issue

--- StudentManagementSystem/StudentManagementSystem/settings.py
+++ StudentManagementSystem/StudentManagementSystem/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/ 'app/static']
+STATICFILES_DIRS = [BASE_DIR / 'app/../static']
 STATIC_ROOT = BASE_DIR / 'static'
 
 
